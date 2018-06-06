18129a69168aff09764579adf8ff414fe65efb1b
Artur <wizeek80@gmail.com>
2018-06-07 00:27:25 +0300

    added alert on failed bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+    .catch(error => {
+        console.log(error);
+        window.alert(`Bootstrap failed. Cause: ${error}`);
+    });
